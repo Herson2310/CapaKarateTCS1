079d10a14f65610332cab945924bab120278062a
Herson2310 <hersonvch23@gmail.com>
2025-10-12 19:44:30 -0500

    agregue ejemplos base de datos

--- src/test/java/utilities/Database.java
+++ src/test/java/utilities/Database.java
@@ -1,14 +1,17 @@
 package utilities;
 
+import net.minidev.json.JSONObject;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Database {
 
     private static String connectionUrl = "jdbc:mysql://localhost:3306/capacitacion?user=root&password=";
 
-    public static void add(String dni, String nombre, String apellido, String servicio, double deuda) {
+    public static void agregarCliente(String dni, String nombre, String apellido, String servicio, double deuda) {
 
         try (Connection connect = DriverManager.getConnection(connectionUrl)) {
 
@@ -19,4 +22,28 @@ public class Database {
             e.printStackTrace();
         }
     }
+
+    public static JSONObject obtenerRegistro(String dni) {
+
+        JSONObject json = new JSONObject();
+
+        try (Connection connect = DriverManager.getConnection(connectionUrl)) {
+
+            ResultSet rs = connect.createStatement().executeQuery("SELECT * FROM clientes WHERE dni='" + dni + "'");
+            rs.next();
+
+            json.put("DNI", rs.getString("dni"));
+            json.put("NOMBRE", rs.getString("nombre"));
+            json.put("APELLIDOS", rs.getString("apellidos"));
+            json.put("SERVICIO", rs.getString("servicio"));
+            json.put("DEUDA", rs.getString("deuda"));
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+
+        return json;
+    }
+
 }
--- target/test-classes/features/database/workDB.feature
+++ target/test-classes/features/database/workDB.feature
@@ -1,8 +1,27 @@
-Feature: validar tablas en la base de datos
+Feature: validar base de datos
 
   Background: connect to db
     * def db = Java.type('utilities.Database')
 
   @db_001
   Scenario: insertar nuevo registro tabla cliente
-    * eval db.add("85285285","Dina","Paucar Gonzales", "Agua", 140.20)
+    * eval db.agregarCliente("74174174","Liz","Lobaton Klug", "Gas", 63.50)
+
+  @db_002
+  Scenario Outline: insertar registros tabla cliente
+    * eval db.agregarCliente("<dni>","<nombre>","<apellidos>", "<servicio>", <deuda>)
+
+    Examples:
+      | dni      | nombre  | apellidos      | servicio | deuda  |
+      | 56565656 | Julieta | Lascano Ugarte | Telefono | 15.23  |
+      | 57575757 | Tito    | Lara Smith     | Cable    | 168.10 |
+
+  @db_003
+  Scenario: Obtener campos de un registro por el dni
+    * def cliente = db.obtenerRegistro("57575757")
+    * print 'DNI: ', cliente.DNI
+    * print 'Nombre: ', cliente.NOMBRE
+    * print 'Apellidos: ', cliente.APELLIDOS
+    * print 'Servicio: ', cliente.SERVICIO
+    * print 'Deuda: ', cliente.DEUDA
+    * match cliente.NOMBRE == 'Tito'
